7b6808cb33b99f69a4f6a733e599e109b58fe66c
daniela-arias-ordonez <daniela.arias2000@gmail.com>
2019-07-24 14:31:24 -0500

    MVP git push origin daniela-arias-ordonez git commit -m MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-
 import SavedList from './Movies/SavedList';
 import Movie from './Movies/Movie';
 import MovieList from './Movies/MovieList';
-import Route from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -16,8 +15,8 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>
-      <Route path="/" component={MovieList} />
-      <Route path="/movies/:movieID" component={Movie}/>
+      <Route exact path="/" component={MovieList} />
+      <Route exact path="/movies/:id" component={Movie}/>
       </div>
     </div>
   );
